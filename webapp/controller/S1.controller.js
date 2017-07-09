22e69e96667ea5578f8f8903e566440bd1d06a88
Fabio Pagoti <fabiopagoti@gmail.com>
2017-07-09 14:20:24 +0000

    rotas implementadas

--- webapp/controller/S1.controller.js
+++ webapp/controller/S1.controller.js
@@ -1,45 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent"
+], function(Controller, UIComponent) {
 	"use strict";
 
 	return Controller.extend("demo.odata.controller.S1", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf demo.odata.view.S1
-		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+			this._oRouter = UIComponent.getRouterFor(this);
+		},
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf demo.odata.view.S1
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf demo.odata.view.S1
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf demo.odata.view.S1
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		onListItemPress: function(oEvent) {
+			var oListItem = oEvent.getSource();
+			var oContext = oListItem.getBindingContext();
+			var oCategory = oContext.getObject();
+			var sCategoryId = oCategory.ID;
+			
+			
+			this._oRouter.navTo('products',{
+				category_id: sCategoryId
+			});
+		}
 
 	});
 
 });
+
+
+
