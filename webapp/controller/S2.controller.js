c7ae988bb87917f24ef69ccb04ca5df3992fc769
Fabio Pagoti <fabiopagoti@gmail.com>
2017-07-09 01:59:18 +0000

    bind element feito

--- webapp/controller/S2.controller.js
+++ webapp/controller/S2.controller.js
@@ -1,44 +1,30 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, UIComponent, JSONModel) {
 	"use strict";
 
 	return Controller.extend("demo.odata.controller.S2", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf demo.odata.view.S2
-		 */
-		//	onInit: function() {
-		//
-		//	},
+		onInit: function() {
+			this._oRouter = UIComponent.getRouterFor(this);
+			this._oList = this.byId('product_list');
 
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf demo.odata.view.S2
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
+			this._oRouter.getRoute('products').attachPatternMatched(this._onPatternMatched, this);
 
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf demo.odata.view.S2
-		 */
-		//	onAfterRendering: function() {
-		//
-		//	},
+		},
 
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf demo.odata.view.S2
-		 */
-		//	onExit: function() {
-		//
-		//	}
+		_onPatternMatched: function(oEvent) {
+			var oParameters = oEvent.getParameters();
+			var oArguments = oParameters.arguments;
+			var sCategoryId = oArguments.id;
+
+			this.getView().bindElement({
+				path: `/Categories(${sCategoryId})`,
+			});
+
+		}
 
 	});
 
